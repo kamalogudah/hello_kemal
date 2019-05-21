890551681081d54db3450fd84727e08ba623e578
kamalogudah <mcpaul2058@gmail.com>
2019-05-21 20:05:53 +0300

    Views/Templates

--- src/hello_kemal.cr
+++ src/hello_kemal.cr
@@ -6,5 +6,15 @@ require "kemal"
   get "/" do
     "Hello World!"
   end
+
+  # get "/:name" do |env|
+  #   name = env.params.url["name"]
+  #   render "src/views/hello.ecr"
+  # end
+
+  get "/:name" do |env|
+    name = env.params.url["name"]
+    render "src/views/hello.ecr", "src/views/layout/layout.ecr"
+  end
   Kemal.config.port = 3001
   Kemal.run
